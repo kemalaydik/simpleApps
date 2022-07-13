4efb0ab09bab5191ac44d965d34d9c8be47a8739
Kemal Aydık <kemalaydik@gmail.com>
2022-07-13 11:30:12 +0300

    complete modal

---
+++ modal/dist/script.js
@@ -0,0 +1,4 @@
+const modal = document.getElementById('defaultModal');
+const toggleBtn = document.querySelector('button');
+
+toggleBtn.addEventListener('click', () => modal.classList.toggle('hidden'));
